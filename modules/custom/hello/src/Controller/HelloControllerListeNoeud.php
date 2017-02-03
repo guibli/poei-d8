4f5c05e18685273df518f2d2b7b54311f5ddc563
guibli <guillaume.Bleriot@gmail.com>
2017-02-03 12:29:25 +0100

    cleaner query

--- modules/custom/hello/src/Controller/HelloControllerListeNoeud.php
+++ modules/custom/hello/src/Controller/HelloControllerListeNoeud.php
@@ -3,28 +3,22 @@
 namespace Drupal\hello\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-
+use \Drupal\Core\Link;
 class HelloControllerListeNoeud extends ControllerBase{
 
   public function content($entitytype = NULL ){
 
     $storage = \Drupal::entityTypeManager()->getStorage('node');
-
-    if($entitytype != NULL){
-      $ids = \Drupal::entityQuery('node')
-        ->pager()
-        ->condition('type', $entitytype)
-        ->execute();
-    }else{
-      $ids = \Drupal::entityQuery('node')
-        ->pager()
-        ->execute();
+    $query = \Drupal::entityQuery('node');
+    if($entitytype) {
+      $query->condition('type', $entitytype);
     }
-
+    $ids = $query->pager(15)->execute();
     $entities = $storage->loadMultiple($ids);
     
     foreach ($entities as $item) {
-      $list[]=$item->toLink($item->getTitle());
+      $list[] = Link::createFromRoute($item->label(),'entity.node.canonical',array('node'=>$item->id()));
+      // $list[]=$item->toLink($item->getTitle());
     }
 
     $render[]=array(
